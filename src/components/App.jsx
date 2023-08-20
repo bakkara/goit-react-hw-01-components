88a4c3699f003de87006dd1115dc075a6fdab645
bakkara <ane4karymar@gmail.com>
2023-08-20 14:48:00 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,17 @@ import { FriendList } from "./FriendList/FriendList"
 import user from '../user.json'
 import data from '../data.json'
 import friends from '../friends.json'
+import transactions from '../transactions.json'
+import { TransactionHistory } from "./TransactionHistory/TransactionHistory"
 
 export const App = () => {
   return (
     <Layout>
-      <Profile items={user} />
+      <Profile items={user}/>
       <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
       <FriendList friends={friends} />
-
+      <TransactionHistory items={transactions} />
     </Layout>
   )
 }
